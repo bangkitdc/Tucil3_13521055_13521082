55b8c691c89b0a4ef121fffdef913e6f9f7887f1
bangkitdc <bangkitdc@gmail.com>
2023-04-10 02:29:34 +0700

    feat: integrate UI with maps

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ import customtkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import UCS
 import ASTAR
-
-def btn_clicked():
-    print("Button Clicked")
+from tkintermapview import TkinterMapView
+from geopy.distance import geodesic
+from math import radians, sin, cos, sqrt, atan2
 
 class GraphVisualizer:
     def __init__(self):
@@ -19,8 +19,14 @@ class GraphVisualizer:
         self.window.configure(bg = "#FFFFFF")
         self.window.resizable(False, False)
 
+        self.bonus = False
+        self.countMarker = 0
+        self.savedcoordinates = 0
+        self.is_coordinate_saved = False
+        self.is_path_enabled = False
+
         self.G = None
-        self.coords = {}
+        self.node_coords = {}
         self.filename = None
 
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -81,7 +87,7 @@ class GraphVisualizer:
             image = img2,
             borderwidth = 0,
             highlightthickness = 0,
-            command = btn_clicked,
+            command = self.check_bonus,
             relief = "flat")
 
         self.b2.place(
@@ -210,12 +216,9 @@ class GraphVisualizer:
             )
 
         # Create canvas for matplotlib graph
-        self.canvas_plot = Canvas(canvas, width=560, height=400, bg='#A5C9CA')
-        self.canvas_plot.place(
-            x = 452, y = 110,
-            )
+        self.setup_plot()
 
-        self.window.mainloop()
+        self.window.mainloop()        
 
     def combobox_start(self, choice):
         self.start_node = choice
@@ -280,10 +283,10 @@ class GraphVisualizer:
         self.pos = nx.spring_layout(self.G)
 
         # Loop through nodes and get their positions
-        self.coords = {}
+        self.node_coords = {}
         for node in self.G.nodes():
             x, y = self.pos[node]
-            self.coords[node] = (float(x), float(y))
+            self.node_coords[node] = (float(x), float(y))
 
         nx.draw_networkx_nodes(self.G, self.pos, node_color='#1f78b4')
         nx.draw_networkx_edges(self.G, self.pos, edgelist=self.G.edges(), edge_color='lightgray')
@@ -307,7 +310,7 @@ class GraphVisualizer:
 
     def check_draw_graph(self):
         # Check filename
-        if self.filename == None:
+        if self.filename == None and self.bonus == False:
             messagebox.showwarning("No File Input", "You have to input txt file first!")
             return   
 
@@ -321,39 +324,98 @@ class GraphVisualizer:
             messagebox.showwarning("No Algorithm Input", "You have to select the algorithm first!")
             return
 
-        self.draw_graph()
+        if (self.bonus):
+            shortest_path = self.get_shortest_path()
+
+            if (shortest_path == -1):
+                return
+
+            total_weight = 0
+            # Update graph
+            for i in range(len(shortest_path)-1):
+                path = self.map_widget.set_path([self.node_coords[shortest_path[i]], self.node_coords[shortest_path[i + 1]]], color = "green")
+                total_weight += self.get_distance(self.node_coords[shortest_path[i]], self.node_coords[shortest_path[i + 1]])
+
+            # Display Cost
+            self.entry1.delete(0, END)
+            self.entry1.insert(0, str(round(total_weight, 5)) + " km")
+
+            # Display Route
+            path_str = ' → '.join(shortest_path)
+            self.entry2.delete(0, END)
+            self.entry2.insert(0, str(path_str))
+
+        else:
+            self.draw_graph()
+
+    def get_distance(self, coord1, coord2):
+        # Approximate radius of earth in km
+        R = 6373.0
+
+        lat1, lon1 = coord1
+        lat2, lon2 = coord2
+
+        # Convert coordinates to radians
+        lat1 = radians(lat1)
+        lon1 = radians(lon1)
+        lat2 = radians(lat2)
+        lon2 = radians(lon2)
+
+        # Calculate the difference between the coordinates
+        delta_lon = lon2 - lon1
+        delta_lat = lat2 - lat1
+
+        # Apply the haversine formula
+        a = sin(delta_lat / 2)**2 + cos(lat1) * cos(lat2) * sin(delta_lon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        # Calculate the distance in kilometers
+        distance = R * c
+
+        return distance
+
+    def get_shortest_path(self):
+        # Get the shortest path between the start and end nodes
+        # UCS
+        if (self.algorithm_used == 1):
+            try:
+                shortest_path = UCS.UCS(self.G, self.start_node, self.target_node)
+                return shortest_path
+            except nx.NetworkXNoPath:
+                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
+                return -1
+            except ValueError:
+                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
+                return -1
+        # A-Star
+        else:
+            try:
+                shortest_path = ASTAR.ASTAR(self.G, self.start_node, self.target_node, self.node_coords)
+                return shortest_path
+            except nx.NetworkXNoPath:
+                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
+                return -1
+            except ValueError:
+                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
+                return -1
         
     def draw_graph(self):
         # Read adjacency matrix from file
         with open(self.filename) as f:
             lines = f.readlines()
 
-        # Get the shortest path between the start and end nodes
-        # UCS
-        if (self.algorithm_used == 1):
-            try:
-                shortest_path = UCS.UCS(self.G, self.start_node, self.target_node)
-            except nx.NetworkXNoPath:
-                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
-                return
-            except ValueError:
-                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
-        # A-Star
-        else:
-            try:
-                shortest_path = ASTAR.ASTAR(self.G, self.start_node, self.target_node, self.coords)
-            except nx.NetworkXNoPath:
-                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
-                return
-            except ValueError:
-                messagebox.showwarning("No Path Found", "There is no path between the start and end nodes.")
-        
-        # Update the graph
+        shortest_path = self.get_shortest_path()
+
+        if (shortest_path == -1):
+            return
+
+        # Add path edges
         path_edges = [(u, v) for u, v in zip(shortest_path, shortest_path[1:])]
 
         # Clear
         plt.clf()
-        
+
+        # Update the graph
         node_color = ['red' if node in shortest_path else '#1f78b4' for node in self.G.nodes()]
         nx.draw_networkx_nodes(self.G, self.pos, node_color=node_color)
         nx.draw_networkx_edges(self.G, self.pos, edgelist=self.G.edges(), edge_color='lightgray')
@@ -369,11 +431,155 @@ class GraphVisualizer:
         self.entry1.insert(0, str(total_weight))
 
         # Display Route
-        path_str = ' -> '.join(shortest_path)
+        path_str = ' → '.join(shortest_path)
         self.entry2.delete(0, END)
         self.entry2.insert(0, str(path_str))
         
         self.display_graph()
+
+    def setup_plot(self):
+        self.canvas_plot = Canvas(self.window, width=560, height=400, bg='#A5C9CA', highlightthickness=0, borderwidth=0)
+        self.canvas_plot.place(
+            x = 452, y = 110,
+            )
+
+    def check_bonus(self):
+        if (self.bonus):
+            self.canvas_temp.destroy()
+            self.setup_plot()
+            self.bonus = False
+        else:
+            self.setup_map_view()
+
+    def setup_map_view(self):
+        self.bonus = True
+        self.G = nx.Graph()
+        self.canvas_temp = Canvas(self.window, width=560, height=400, bg='#E7F6F2', highlightthickness=0, borderwidth=0)
+        self.canvas_temp.place(
+            x = 452, y = 110,
+            )
+
+        # Create map widget
+        self.map_widget = TkinterMapView(self.canvas_temp, width=560, height=380, corner_radius=0)
+        self.map_widget.place(
+            x = 0, y = 35,
+            )
+
+        self.entry_search = customtkinter.CTkEntry(master=self.canvas_temp,
+                                            placeholder_text="type address ...",
+                                            width=200,
+                                            font=("Poppins", 14))
+
+        self.entry_search.place(
+            x = 0, y = 0
+            )
+
+        self.entry_search.bind("<Return>", self.search_event)
+
+        self.button_search = customtkinter.CTkButton(master=self.canvas_temp,
+                                                    text="Search",
+                                                    width=90,
+                                                    command=self.search_event,
+                                                    fg_color="#395B64",
+                                                    hover_color="#53757E",
+                                                    font=("Poppins", 14))
+
+        self.button_search.place(
+            x = 210, y = 0
+            )
+
+        self.map_option_menu = customtkinter.CTkOptionMenu(self.canvas_temp, values=["OpenStreetMap", "Google normal", "Google satellite"],
+                                                                             command=self.change_map,
+                                                                             bg_color="#E7F6F2",
+                                                                             fg_color="#395B64",
+                                                                             button_color="#2C3333",
+                                                                             button_hover_color="#464D4D",
+                                                                             dropdown_fg_color="#395B64",
+                                                                             dropdown_hover_color="#2C3333",
+                                                                             font=("Poppins", 14),
+                                                                             dropdown_font=("Poppins", 14)
+                                                                             )
+
+        self.map_option_menu.place(
+            x = 310, y = 0
+            )
+
+        self.button_clear = customtkinter.CTkButton(master=self.canvas_temp,
+                                                    text="Clear Marker",
+                                                    width=100,
+                                                    command=self.clear_marker_event,
+                                                    fg_color="#395B64",
+                                                    hover_color="#53757E",
+                                                    font=("Poppins", 14))
+
+        self.button_clear.place(
+            x = 460, y = 0
+            )
+
+        self.map_widget.add_right_click_menu_command(label="Add Marker",
+                                                    command=self.add_marker_event,
+                                                    pass_coords=True)
+
+        self.map_widget.add_right_click_menu_command(label="Toggle Pathmaking",
+                                                    command=self.enable_path,
+                                                    pass_coords=False)
+
+        # Set default values
+        self.map_widget.set_address("ITB, Bandung")
+        self.map_widget.set_zoom(17)
+
+    def add_marker_event(self, coords):
+        print("Add marker:", coords)
+        self.countMarker += 1
+        node_name = str(self.countMarker)
+        new_marker = self.map_widget.set_marker(coords[0], coords[1], text=str(self.countMarker), command=self.marker_clicked)
+        self.G.add_node(node_name)
+        self.node_coords[node_name]=coords
+
+        marker_list = [str(i) for i in range(1, self.countMarker+1)]
+
+        self.combobox0.configure(state="normal", values=marker_list)
+        self.combobox1.configure(state="normal", values=marker_list)
+
+    # Function triggered when a marker is clicked
+    def marker_clicked(self, marker):
+        if self.is_path_enabled:
+            if not self.is_coordinate_saved:
+                self.savedcoordinates = marker.position
+                self.is_coordinate_saved = True
+            else:
+                if marker.position != self.savedcoordinates:
+                    c1 = marker.position
+                    c2 = self.savedcoordinates
+                    path = self.map_widget.set_path([c1, c2])
+                    self.G.add_edge(self.getKey(c1), self.getKey(c2), weight=geodesic(c1,c2).km)
+                    self.is_coordinate_saved = False
+
+    # Enable pathmatking option when right click
+    def enable_path(self):
+        self.is_path_enabled = not self.is_path_enabled
+
+    # Get a node index given a coordinate
+    def getKey(self, coordinate):
+        for key in self.node_coords:
+            if self.node_coords[key] == coordinate:
+                return key
+        return -1
+
+    def clear_marker_event(self):
+        self.map_widget.delete_all_marker()
+        self.countMarker = 0
+
+    def search_event(self, event=None):
+        self.map_widget.set_address(self.entry_search.get())
+
+    def change_map(self, new_map: str):
+        if new_map == "OpenStreetMap":
+            self.map_widget.set_tile_server("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        elif new_map == "Google normal":
+            self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+        elif new_map == "Google satellite":
+            self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
 
 # Run App
 if __name__ == "__main__":
